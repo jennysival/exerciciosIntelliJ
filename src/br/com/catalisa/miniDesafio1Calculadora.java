3f5513750bd0edb9b435c4ffcd7ee5c953b82825
jennysival <jeniffersival@gmail.com>
2022-04-07 17:07:45 -0300

    terminar a calculadora

--- src/br/com/catalisa/miniDesafio1Calculadora.java
+++ src/br/com/catalisa/miniDesafio1Calculadora.java
@@ -12,16 +12,37 @@ public class miniDesafio1Calculadora {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("Digite o 1º número: ");
-        float num1 = leitor.nextInt();
+        float numDig1 = leitor.nextInt();
 
         System.out.println("Digite o 2º número: ");
-        float num2 = leitor.nextInt();
+        float numDig2 = leitor.nextInt();
 
+        System.out.println("-------------------------------------");
         System.out.println("Escolha qual cálculo deseja realizar:");
         System.out.println("1. Adição(+)");
         System.out.println("2. Subtração(-)");
         System.out.println("3. Multiplicação(x)");
         System.out.println("4. Divisão");
+        System.out.println("-------------------------------------");
+        System.out.println("Digite apenas o nº da opção escolhida:");
+        int opc = leitor.nextInt();
+
+        if(opc == 1){
+            adicao(numDig1, numDig2);
+        }
+        else if(opc == 2){
+            subtracao(numDig1, numDig2);
+        }
+        else if(opc == 3){
+            multiplicacao(numDig1, numDig2);
+        }
+        else if (opc ==4){
+            divisao(numDig1, numDig2);
+        }
+        else{
+            System.out.println("Seleção inválida");
+        }
+
 
 
     }
@@ -29,6 +50,44 @@ public class miniDesafio1Calculadora {
     public static void adicao(float num1, float num2){
         float soma = num1+num2;
         System.out.println(num1+" + "+num2+" = "+soma);
+
+        botaoSair();
+    }
+
+    public static void subtracao(float num1, float num2){
+        float subt = num1-num2;
+        System.out.println(num1+" - "+num2+" = "+subt);
+
+        botaoSair();
+    }
+
+    public static void multiplicacao(float num1, float num2){
+        float multi = num1*num2;
+        System.out.println(num1+" x "+num2+" = "+multi);
+
+        botaoSair();
+    }
+
+    public static void divisao(float num1, float num2){
+        float divi = num1/num2;
+        System.out.println(num1+" / "+num2+" = "+divi);
+
+        botaoSair();
+    }
+
+    public static void botaoSair(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("- Digite [1] para sair; ");
+        int opc = leitor.nextInt();
+
+        if(opc == 1){
+            System.exit(0);
+        }
+        else{
+            System.out.println("Seleção inválida. Tente novamente");
+            botaoSair();
+        }
+
     }
 
 }
